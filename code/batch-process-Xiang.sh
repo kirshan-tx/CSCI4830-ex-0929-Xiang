0229e1889e7abb163785c1bb504d91ddc0074068
Kirshan Tzer Xiang <kirshantzerxiang@gmail.com>
2023-09-29 15:43:02 -0500

    Create batch-process-Xiang.sh

---
+++ code/batch-process-Xiang.sh
@@ -0,0 +1,13 @@
+#! /bin/sh 
+for f in "$1"/*; do
+  if [ -f "$f" ]; then
+    SIZE="$(du -sh "${f}" | cut -f1)"
+    WORD_COUNT="$(wc -w "${f}" | cut -d' ' -f1)"
+    
+    echo "Processing $f file..."
+    echo "File size: $SIZE"
+    echo -n "Word count: $WORD_COUNT"
+    echo
+  fi
+done
+
